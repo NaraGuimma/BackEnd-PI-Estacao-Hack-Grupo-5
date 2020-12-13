7aff5b115713f9df0842cf1b11f870142f2877b2
NaraGuimma <naraguimma@gmail.com>
2020-12-13 16:39:04 -0300

    Backend registro de users do E-commerce, grupo 5 Estação Hack

---
+++ src/controllers/UsersController.ts
@@ -0,0 +1,44 @@
+import { Request, Response } from 'express';
+import db from '../database/connection';
+export default class UsersController {
+  async index(request: Request, response: Response) {
+    const filters = request.query;
+
+    const email = filters.email as string;
+
+    if (!filters.email) {
+      return response.status(400).json({
+        error: 'Missing filters to search users',
+      });
+    }
+
+    const users = await db('users')
+      .where('users.email', '=', email)
+      .select('users.*');
+
+    return response.json(users);
+  }
+
+  async create(request: Request, response: Response) {
+    const { name, email, password } = request.body;
+
+    const trx = await db.transaction();
+
+    try {
+      await trx('users').insert({
+        name,
+        email,
+        password,
+      });
+
+      await trx.commit();
+      return response.status(201).send();
+    } catch (err) {
+      await trx.rollback();
+      console.log(err);
+      return response.status(400).send({
+        error: 'Unexpected error while creating new user',
+      });
+    }
+  }
+}
---
+++ src/routes.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+import UsersController from './controllers/UsersController';
+const routes = express.Router();
+const usersControllers = new UsersController();
+
+routes.get('/users', usersControllers.index);
+routes.post('/users', usersControllers.create);
+
+export default routes;
