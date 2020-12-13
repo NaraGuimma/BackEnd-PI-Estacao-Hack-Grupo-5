1e87e23df4cd12bff894cf9d9c381f79b7387317
NaraGuimma <naraguimma@gmail.com>
2020-12-13 16:53:31 -0300

    fazendo alterações no server.ts

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,9 @@ import express from 'express';
 import routes from './routes';
 const cors = require('cors');
 const app = express();
-
+const PORT: string | number = process.env.PORT || 3333;
 app.use(cors());
 app.use(express.json());
 app.use(routes);
-app.listen(3333);
+
+app.listen(PORT, () => console.log(`hosting @${PORT}`));
